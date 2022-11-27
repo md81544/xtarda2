8392e5ba47c1298ca0a78c9c1ffd8d113ce2f73a
Martyn Davis <mcd@marengo.cc>
2022-11-27 02:41:35 +0000

    Initial implementation of mothership drawing / movement

---
+++ src/game.rs
@@ -0,0 +1,51 @@
+use sfml::graphics::{
+    RenderTarget, RenderWindow, Shape, Transformable, Color, RectangleShape};
+use sfml::system::Vector2f;
+
+pub struct Game {
+    _level: u8,
+    window_width: u32,
+    _window_height: u32,
+    mothership_pos_y: f32,
+    mothership_pos_x: f32,
+    mothership_direction: i8,
+}
+
+impl Game {
+    pub fn new(window_width: u32, _window_height: u32) -> Game {
+        Game{
+            _level: 1,
+            window_width,
+            _window_height,
+            mothership_pos_x: 50.0,
+            mothership_pos_y: 50.0,
+            mothership_direction: 20,
+        }
+    }
+
+    fn draw_mothership(&mut self, window: &mut RenderWindow) {
+        let mut fin =  RectangleShape::with_size(Vector2f::new(15.0, 15.0));
+        fin.set_fill_color(Color::rgb(0,255,0));
+        fin.set_position(Vector2f::new(self.mothership_pos_x, self.mothership_pos_y + 15.0));
+        window.draw(&fin);
+        fin.set_position(Vector2f::new(self.mothership_pos_x + 65.0, self.mothership_pos_y + 15.0));
+        window.draw(&fin);
+        let mut body =  RectangleShape::with_size(Vector2f::new(50.0, 30.0));
+        body.set_fill_color(Color::rgb(0,255,0));
+        body.set_position(Vector2f::new(self.mothership_pos_x + 15.0, self.mothership_pos_y));
+        window.draw(&body);
+    }
+
+    pub fn draw_screen(&mut self, window: &mut RenderWindow) {
+        self.draw_mothership(window);
+    }
+
+    pub fn iterate(&mut self) {
+        if (self.mothership_pos_x > (self.window_width - 150) as f32 &&
+                self.mothership_direction > 0)||
+           (self.mothership_pos_x < 50.0 && self.mothership_direction < 0 ) {
+            self.mothership_direction = -self.mothership_direction;
+        }
+        self.mothership_pos_x += self.mothership_direction as f32;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use sfml::graphics::{ Color, RenderTarget, RenderWindow };
+use sfml::system::{Vector2i};
+use sfml::window::{ContextSettings, Event, Key, Style, VideoMode};
+
+mod game;
+
+fn main() {
+
+    let screen_width  = VideoMode::desktop_mode().width;
+    let screen_height = VideoMode::desktop_mode().height;
+    let ratio: f32 = screen_width as f32 / screen_height as f32;
+
+    let window_width = if screen_width >= 1920 { 1920 } else { screen_width };
+    let window_height = ( window_width as f32 /ratio ) as u32;
+
+    let mut window = RenderWindow::new(
+        (window_width, window_height),
+        "Xtarda Rescue",
+        Style::DEFAULT,
+        &ContextSettings::default(),
+    );
+    window.set_framerate_limit(60);
+    window.set_vertical_sync_enabled(true);
+    window.set_position(Vector2i::new(50, 50));
+    window.set_mouse_cursor_visible(false);
+
+    let mut game = game::Game::new(window_width, window_height);
+
+    // Main Loop
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed => window.close(),
+                Event::KeyReleased { code, .. } => {
+                    match code {
+                        Key::Escape => {
+                            window.close();
+                        },
+                        Key::Q => {
+                            window.close();
+                        },
+                        _ => {}
+                    }
+                },
+                _ => {} // ignore other events
+            }
+        }
+        window.clear(Color::BLACK);
+        game.iterate();
+        game.draw_screen(&mut window);
+        window.display();
+    }
+}
